db200dfb0e9430ed62d6112144ca53893b544105
tgromysz <tgromysz@gmail.com>
2024-07-26 12:58:01 +0200

    version 1.0

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	// Calculator class instance
+	public static Calculator calculator;
+
+	// Entry point
+	public static void main(String[] args) {
+		calculator = new Calculator();
+	}
+}
